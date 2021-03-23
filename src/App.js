e808b943f709f8827c4003a92f53481f12854e8a
Dave Roverts <me@daveroverts.nl>
2021-03-23 19:38:04 +0100

    Let's wrap things in cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Container, Grid, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import "./App.css";
 import AircraftForm from "./components/AircraftForm";
 import AircraftList from "./components/AircraftList";
@@ -9,44 +15,58 @@ function App() {
   const [arrivals, setArrivals] = useState([]);
   return (
     <Container>
-      <Typography variant="h2">
-        CPT Movements Tracker
-      </Typography>
-
+      <Typography variant="h2">CPT Movements Tracker</Typography>
 
       <Grid container spacing={10}>
-        <Grid item>
-        <Typography variant="h3">
-        Departures
-      </Typography>
-      <AircraftForm saveAircraft={(aircraft) => {
-        const trimmedAircraft = aircraft.trim();
-        if (trimmedAircraft.length > 0) {
-          setDepartures([...departures, trimmedAircraft])
-        }
-      }} />
-      <AircraftList aircraft={departures} deleteAircraft={(aircraftIndex) => {
-        const newAircraft = departures.filter((_, index) => index !== aircraftIndex)
-        setDepartures(newAircraft)
-      }} />
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography variant="h3">Departures</Typography>
+              <AircraftForm
+                saveAircraft={(aircraft) => {
+                  const trimmedAircraft = aircraft.trim();
+                  if (trimmedAircraft.length > 0) {
+                    setDepartures([...departures, trimmedAircraft]);
+                  }
+                }}
+              />
+              <AircraftList
+                aircraft={departures}
+                deleteAircraft={(aircraftIndex) => {
+                  const newAircraft = departures.filter(
+                    (_, index) => index !== aircraftIndex
+                  );
+                  setDepartures(newAircraft);
+                }}
+              />
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item>
-        <Typography variant="h3">
-        Arrivals
-      </Typography>
-      <AircraftForm saveAircraft={(aircraft) => {
-        const trimmedAircraft = aircraft.trim();
-        if (trimmedAircraft.length > 0) {
-          setArrivals([...arrivals, trimmedAircraft])
-        }
-      }} />
-      <AircraftList aircraft={arrivals} deleteAircraft={(aircraftIndex) => {
-        const newAircraft = arrivals.filter((_, index) => index !== aircraftIndex)
-        setArrivals(newAircraft)
-      }} />
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography variant="h3">Arrivals</Typography>
+              <AircraftForm
+                saveAircraft={(aircraft) => {
+                  const trimmedAircraft = aircraft.trim();
+                  if (trimmedAircraft.length > 0) {
+                    setArrivals([...arrivals, trimmedAircraft]);
+                  }
+                }}
+              />
+              <AircraftList
+                aircraft={arrivals}
+                deleteAircraft={(aircraftIndex) => {
+                  const newAircraft = arrivals.filter(
+                    (_, index) => index !== aircraftIndex
+                  );
+                  setArrivals(newAircraft);
+                }}
+              />
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
-
     </Container>
   );
 }
