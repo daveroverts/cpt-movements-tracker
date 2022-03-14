627df1a6c93651013e2ad4850c2fd014e74faa59
Dave Roverts <me@daveroverts.nl>
2022-03-14 22:57:17 +0100

    chore: add start of index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import { Anchor, Container, Group, Title } from "@mantine/core";
+import Copyright from "components/Copyright";
+import Link from "next/link";
+
 export default function Home() {
-  return <div />;
+  return (
+    <Container>
+      <Group direction="column">
+        <Title>CPT Movements Tracker</Title>
+
+        <Link href="/about" passHref>
+          <Anchor>Go to the about page</Anchor>
+        </Link>
+        <Copyright />
+      </Group>
+    </Container>
+  );
 }
