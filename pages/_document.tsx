bcb74f9cb85556bf4e2884791860ae848bfcee71
Dave Roverts <me@daveroverts.nl>
2022-03-18 19:54:19 +0100

    feat(_document): add lang tag and support ssr styles extraction

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,38 @@
-import Document from "next/document";
-import { createGetInitialProps } from "@mantine/next";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyles, createStylesServer } from "@mantine/next";
 
-const getInitialProps = createGetInitialProps();
+const stylesServer = createStylesServer();
 
 export default class MyDocument extends Document {
-  static getInitialProps = getInitialProps;
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
